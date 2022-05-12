6bf6552f9ab0a3dd4852e126d8e1a4741367ed38
JanGaud <JanisGaudreault.jg@gmail.com>
2022-05-12 15:12:34 -0400

    Create JeuxCartes.php
    Initialisation du jeyux de cartes

---
+++ Cartes/JeuxCartes.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        $cartes_nombres = [2, 3, 4, 5, 6, 7, 8, 9, 10, 'Valet', 'Dame', 'Roi', 'As'];
+        $cartes_figures = ["&#9830", "&#9827", "&#9829", "&#9824"];
+        $jeu_complet = [];
+        $paquet1 = [];
+        $paquet2 = [];
+        $paquet_brasse = [];
+
+    ?>
+</body>
+</html>
